61c4761934055b62ae5f4b03f7d2d264919dc560
Puja mangalpalli <pmsm901@gmail.com>
2026-08-23 12:59:17 +0530

    adding cartschema in cart model

---
+++ models/Cart.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+//creating cart schema or table
+const cartSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product",
+        required:true
+    },
+    quantity:{
+        type:number,
+        required:true,
+        min:[1,"Quantity must be at least 1"]
+    }
+},
+    {
+        timestamps:true
+    }
+);
+
+const Cart=mongoose.model("Cart",cartSchema);
+export default Cart;
